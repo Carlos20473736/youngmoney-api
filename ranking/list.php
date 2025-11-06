bc185da555f88fcef847c64af39f06ef5a219a84
Carlos20473736 <142956294+Carlos20473736@users.noreply.github.com>
2025-11-06 05:33:46 -0300

    Update list.php

--- ranking/list.php
+++ ranking/list.php
@@ -1,11 +1,11 @@
 <?php
-header('Content-Type: application/json' );
+header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200 );
+    http_response_code(200);
     exit;
 }
 
@@ -41,7 +41,7 @@ try {
     ]);
     
 } catch (Exception $e) {
-    http_response_code(500 );
+    http_response_code(500);
     echo json_encode(['success' => false, 'error' => 'Erro interno: ' . $e->getMessage()]);
 }
 ?>
